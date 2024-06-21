0f5a133f4837ddbfd27749a05d3f7f31ff7ed5b8
Daniel Mineiro <daniel.mineiro10@gmail.com>
2024-06-21 00:11:55 -0300

    start project

---
+++ src/main/java/br/com/iouone/IouoneApplication.java
@@ -0,0 +1,13 @@
+package br.com.iouone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IouoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IouoneApplication.class, args);
+	}
+
+}
