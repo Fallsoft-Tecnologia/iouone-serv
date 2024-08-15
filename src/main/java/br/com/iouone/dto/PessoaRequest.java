2e9c7098c09e4d6115eebb41c77b78a88218f699
Hebert Soares <hebertsoaresof@gmail.com>
2024-08-15 11:10:25 -0300

    :sparkles: feat: Cadastro

---
+++ src/main/java/br/com/iouone/dto/PessoaRequest.java
@@ -0,0 +1,70 @@
+package br.com.iouone.dto;
+
+import java.time.LocalDate;
+
+public class PessoaRequest {
+    private String cpf;
+    private String nome;
+    private String email;
+    private String senha;
+    private String celular;
+    private LocalDate dataNascimento;
+    private Integer atividadeFisicaId;
+
+    // Getters e Setters
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public Integer getAtividadeFisicaId() {
+        return atividadeFisicaId;
+    }
+
+    public void setAtividadeFisicaId(Integer atividadeFisicaId) {
+        this.atividadeFisicaId = atividadeFisicaId;
+    }
+}
